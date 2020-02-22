b95d1f1614ea9c58c90ab9480e0f13e233c8bbe6
Dark One <darktz@email.cz>
2020-02-22 13:18:39 +0100

    Finalization

--- Problem00014/src/Main.java
+++ Problem00014/src/Main.java
@@ -3,13 +3,12 @@ public class Main {
     public static void main(String[] args) {
         long firstNumber = 1000000;
         int chain = 0;
-        long resultNumber = 0;
         Computer computer = new Computer(firstNumber, chain);
         chain = computer.getChain();
-        resultNumber = computer.getResultNumber();
+        long resultNumber = computer.getResultNumber();
 
         System.out.println("***********" + " result is " + "***********");
-        System.out.println("Longest sequence had " + chain + " steps.");
-        System.out.println("Origin number responsible for this sequence is: " + resultNumber);
+        System.out.println("Longest sequence had " + chain + " steps.");// Answer: 525
+        System.out.println("Origin number responsible for this sequence is: " + resultNumber);// Answer: 837799
     }
 }
