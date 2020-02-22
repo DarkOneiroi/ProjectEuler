e915f92c03ef1e3533bcf73e80e9cbc4d2c7ff2b
Dark One <darktz@email.cz>
2020-02-22 12:28:17 +0100

    Finalization

--- Problem00001/src/Computer.java
+++ Problem00001/src/Computer.java
@@ -1,7 +1,17 @@
 public class Computer {
+    private int multipleThree;
+    private int multipleFive;
+    private int maxNumber;
     private int result = 0;
 
     public Computer(int multipleThree, int multipleFive, int maxNumber) {
+        this.multipleThree = multipleThree;
+        this.multipleFive = multipleFive;
+        this.maxNumber = maxNumber;
+        compute();
+    }
+
+    private void compute() {
         for (int i = 0; i < maxNumber; i++) {
             if (((i % multipleThree) == 0) || ((i % multipleFive) == 0)) {
                 result += i;
