6fcb546a32e5a9abf3afbaa8a099644895eb4fab
Dark One <darktz@email.cz>
2020-02-22 13:08:44 +0100

    Finalization

--- Problem00006/src/Computer.java
+++ Problem00006/src/Computer.java
@@ -1,7 +1,14 @@
 public class Computer {
-    private int result = 0, squared = 0, sumed = 0, squaredFinal = 0;
+    private int numbers;
+    private int result = 0;
 
     public Computer(int numbers) {
+        this.numbers = numbers;
+        compute();
+    }
+
+    private void compute(){
+        int squared = 0, sumed = 0, squaredFinal = 0;
         for (short i = 1; i < (numbers + 1); i++) {
             squared = i * i;
             squaredFinal += squared;
