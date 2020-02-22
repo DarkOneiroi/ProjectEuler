2292b2c56e13a62dc610cc885379d3a1aea934bd
Dark One <darktz@email.cz>
2020-02-22 12:19:26 +0100

    Finalization

--- Problem00014/src/Computer.java
+++ Problem00014/src/Computer.java
@@ -1,34 +1,38 @@
 public class Computer {
-    int chain;
+    private long firstNumber;
+    private int chain;
+    private long resultNumber;
 
     public Computer(long firstNumber, int chain) {
+        this.firstNumber = firstNumber;
         this.chain = chain;
+        compute();
+    }
+
+    private void compute(){
         for (long i = firstNumber; i > 0; i--) {
             long cacheNumber = i;
             int cacheChain = 0;
-            //System.out.println("We are at i number: " + i);
             while (cacheNumber > 1) {
-                /*if (cacheNumber < 1) {
-                    System.out.println("NEGATIVE ERRORRRRR");
-                }*/
                 if ((cacheNumber % 2) == 0) {
                     cacheNumber = cacheNumber / 2;
                 } else {
                     cacheNumber = 3 * cacheNumber + 1;
-                } /*else {
-                    System.out.println("UNKOWN ERRORRRRR");
-                }*/
+                }
                 cacheChain++;
-                //System.out.println("cacheNumber " + cacheNumber);
             }
             if (cacheChain > this.chain) {
-                this.chain = cacheChain;
-                //System.out.println(chain);
+                this.chain = cacheChain + 1;// + 1 programatorska nula
+                this.resultNumber = i;
             }
         }
     }
 
     public int getChain() {
         return chain;
+    }
+
+    public long getResultNumber() {
+        return resultNumber;
     }
 }
--- Problem00014/src/Main.java
+++ Problem00014/src/Main.java
@@ -3,10 +3,13 @@ public class Main {
     public static void main(String[] args) {
         long firstNumber = 1000000;
         int chain = 0;
+        long resultNumber = 0;
         Computer computer = new Computer(firstNumber, chain);
         chain = computer.getChain();
+        resultNumber = computer.getResultNumber();
 
         System.out.println("***********" + " result is " + "***********");
-        System.out.println(chain);
+        System.out.println("Longest sequence had " + chain + " steps.");
+        System.out.println("Origin number responsible for this sequence is: " + resultNumber);
     }
 }
