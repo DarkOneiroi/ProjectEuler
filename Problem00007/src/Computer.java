a6bf214f688a607835626b2f0e7608b8c1b48c2b
Dark One <darktz@email.cz>
2020-02-22 13:15:12 +0100

    Finalization

--- Problem00007/src/Computer.java
+++ Problem00007/src/Computer.java
@@ -1,9 +1,15 @@
 public class Computer {
-    private long primeNmbr = 0, currentNmbr = 1;
-    private int nmbrOfPrimes = 0, primeCount = 0;
+    private long primeNmbr = 0;
+    private int nmbrOfPrimes;
 
     public Computer(int nmbrOfPrimes) {
         this.nmbrOfPrimes = nmbrOfPrimes;
+        compute();
+    }
+
+    private void compute(){
+        int primeCount = 0;
+        long currentNmbr = 1;
         while (primeCount < this.nmbrOfPrimes) {
             short divisions = 0;
             for (long j = 1; j < currentNmbr; j++) {
--- Problem00007/src/Main.java
+++ Problem00007/src/Main.java
@@ -2,10 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         int nmbrOfPrimes = 10001; //default 10001
-        long primeNmbr = 0;
 
         Computer computer = new Computer(nmbrOfPrimes);
-        primeNmbr = computer.getPrimeNmbr();
+        long primeNmbr = computer.getPrimeNmbr();
 
         System.out.println("Vysledek " + primeNmbr);//Answer 104743
     }
